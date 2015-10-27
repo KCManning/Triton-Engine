c4d726507404678bcdc567c3958ba6154dd183ef
RothranAgnos <rothran.agnos1218@gmail.com>
2015-10-27 13:31:35 -0700

    Animations are now working
    updated teh exporter in the project file and new shader

--- io_scene_xml/__init__.py
+++ io_scene_xml/__init__.py
@@ -25,11 +25,6 @@ class ExportXML(bpy.types.Operator):
 	
 	#Export Settings
 
-	SelectedOnly = BoolProperty(
-		name="Export Selected Objects Only",
-		description="Export only selected objects",
-		default=False)
-
 	CoordinateSystem = EnumProperty(
 		name="Coordinate System",
 		description="Use the selected coordinate system for export",
@@ -43,78 +38,12 @@ class ExportXML(bpy.types.Operator):
 		items=(('Y', "Y", "The Y axis points up"),
 			   ('Z', "Z", "The Z axis points up")),
 		default='Y')
-
-	ExportVertices = BoolProperty(
-		name="Export Mesh Vertices",
-		description="Export mesh vertices",
-		default=True)
-
-	ExportNormals = BoolProperty(
-		name="Export Normals",
-		description="Export mesh normals",
-		default=True)
-
-	ExportUVCoordinates = BoolProperty(
-		name="Export UV Coordinates",
-		description="Export mesh UV coordinates, if any",
-		default=True)
-
+		
 	ExportTangents = BoolProperty(
 		name="Export Tangent Space",
 		description="Export normal tangents and bitangent_signs",
 		default=True)
-
-	ApplyModifiers = BoolProperty(
-		name="Apply Modifiers",
-		description="Apply the effects of object modifiers before export, armature modifiers will not export if skin weights or bones are exported",
-		default=False)
-
-	ExportSkinWeights = BoolProperty(
-		name="Export Skin Weights",
-		description="Bind mesh vertices to armature bones, will not export armature modifiers",
-		default=True)
-
-	ExportArmatureBones = BoolProperty(
-		name="Export Armature Bones",
-		description="Export armatures bones, will not export armature modifiers",
-		default=True)
-
-	ExportRestPose = BoolProperty(
-		name="Export Rest Position",
-		description="Export bones in their rest position works only if exporting Armature Bones too.",
-		default=True)
 		
-	ExportPoses = BoolProperty(
-		name="Export Pose Libraries",
-		description="Cuts the active action in each bone into poses marked by pose_markers, "\
-			"must export Armature Bones to work, otherwise poses won't be exported "\
-			"must have at least one action in pose_library and one pose marker to work",
-		default=True)
-
-	#ExportDefaultAnimations = BoolProperty(
-	#	name="Export Default Animations",
-	#	description="Export object animations. Exports the world matrix of each object relative to it's last matrix each frame, one animation per object, "\
-	#		"you usually don't want to have this enabled",
-	#	default=False)
-		
-	#not final
-	#ExportActions = BoolProperty(
-	#	name="Export Actions",
-	#	description="Export each object's active action, while disabling their nla_tracks. doesn't support multiple actions on one"\
-	#		" object",
-	#	default=False)
-		
-	#ExportNLATracks = BoolProperty(
-	#	name="Export NLA Tracks",
-	#	description="Export Each NLA Track on an object as a solo animation set, use this to export multiple actions as animations of non-armature objects",
-	#	default=False)
-
-	IncludeFrameRate = BoolProperty(
-		name="Include Frame Rate",
-		description="Include the AnimTicksPerSecond template which is "\
-			"used by some engines to control animation speed",
-		default=True)
-
 	def execute(self, context):
 		self.filepath = bpy.path.ensure_ext(self.filepath, ".xml")
 
@@ -131,7 +60,6 @@ class ExportXML(bpy.types.Operator):
 
 def menu_func(self, context):
 	self.layout.operator(ExportXML.bl_idname, text="XML (.xml)")
-
 
 def register():
 	bpy.utils.register_module(__name__)
--- projecttriton/Test/Shaders/phongVertex.glsl
+++ projecttriton/Test/Shaders/phongVertex.glsl
@@ -55,114 +55,46 @@ void main()
 	weight[2] = weights.z;
 	weight[3] = weights.w;
 	
-	vec4 q[4];
-	//q[0] = format(rotations[index[0]]);
-	//q[1] = format(rotations[index[1]]);
-	//q[2] = format(rotations[index[2]]);
-	//q[3] = format(rotations[index[3]]);
-	vec3 t[4];
-	//t[0] = offsets[index[0]];
-	//t[1] = offsets[index[1]];
-	//t[2] = offsets[index[2]];
-	//t[3] = bonePos[index[3]];
-	vec3 b[4];
-	//b[0] = bonePos[index[0]];
-	//b[1] = bonePos[index[1]];
-	//b[2] = bonePos[index[2]];
-	//b[3] = bonePos[index[3]];
-	mat4 boneMat[4];
+	mat4 boneMat[MAX_BONES];
+	
+	for (int i = 0; i < MAX_BONES; ++i)
+	{
+		boneMat[i] = quatToMatrix(format(rotations[i]));
+	}
+	
+	vec4 nVertex[4];
+	vec4 nNormal[4];
+	vec4 nVertex_f = vec4(0.0, 0.0, 0.0, 0.0);
+	vec4 nNormal_f = vec4(0.0, 0.0, 0.0, 0.0);
 	
 	for(int i = 0; i < 4; ++i)
 	{
-		boneMat[i] = quatToMatrix(format(rotations[index[i]]));
-		t[i] = offsets[index[i]];
-		b[i] = bonePos[index[i]];
+		nVertex[i] = vec4(vertex, 0.0);
+		nNormal[i] = vec4(normal, 0.0);
+		
+		vec4 bone_offset = vec4(bonePos[index[i]], 0.0);
+		nVertex[i] -= bone_offset;
+		nVertex[i] = boneMat[index[i]] * nVertex[i];
+		nNormal[i] = boneMat[index[i]] * nNormal[i];
+		nVertex[i] += bone_offset;
+		
+		int pIndex = parentIndices[index[i]];
+		while(pIndex > -1)//pIndex would be -1 for any bone without a parent and -2 for bones that don't exist
+		{
+			vec4 bone_offset = vec4(bonePos[pIndex], 0.0);
+			nVertex[i] -= bone_offset;
+			nVertex[i] = boneMat[pIndex] * nVertex[i];
+			nNormal[i] = boneMat[pIndex] * nNormal[i];
+			nVertex[i] += bone_offset;
+			pIndex = parentIndices[pIndex];
+		}
+		
+		nVertex_f += nVertex[i] * weight[i];
+		nNormal_f += nNormal[i] * weight[i];
 	}
-	
-	vec4 nVertex0 = vec4(vertex - b[0]// - t[0]
-	, 0.0);                          
-	vec4 nVertex1 = vec4(vertex - b[1]// - t[1]
-	, 0.0);                           
-	vec4 nVertex2 = vec4(vertex - b[2]// - t[2]
-	, 0.0);                           
-	vec4 nVertex3 = vec4(vertex - b[3]// - t[3]
-	, 0.0);
-	vec4 nNormal0 = vec4(normal, 0.0);
-	vec4 nNormal1 = vec4(normal, 0.0);
-	vec4 nNormal2 = vec4(normal, 0.0);
-	vec4 nNormal3 = vec4(normal, 0.0);
-	
-	nVertex0 = ((boneMat[0] * nVertex0) + vec4(b[0], 0.0)
-	) * weights.x;
-	nVertex1 = ((boneMat[1] * nVertex1) + vec4(b[1], 0.0)
-	) * weights.y;
-	nVertex2 = ((boneMat[2] * nVertex2) + vec4(b[2], 0.0)
-	) * weights.z;
-	nVertex3 = ((boneMat[3] * nVertex3) + vec4(b[3], 0.0)
-	) * weights.w;
-	nNormal0 = (boneMat[0] * nNormal0) * weights.x;
-	nNormal1 = (boneMat[1] * nNormal1) * weights.y;
-	nNormal2 = (boneMat[2] * nNormal2) * weights.z;
-	nNormal3 = (boneMat[3] * nNormal3) * weights.w;
-	
-	vec4 nVertex_f = vec4((nVertex0 + nVertex1 + nVertex2 + nVertex3).xyz, 1.0);
-	vec4 nNormal_f = nNormal0 + nNormal1 + nNormal2 + nNormal3;
-	
-	//version 1 above here
-	
-	//version 2 currently not working below here
-	
-	//mat4 boneMat[MAX_BONES];
-	//
-	//for (int i = 0; i < MAX_BONES; ++i)
-	//{
-	//	boneMat[i] = quatToMatrix(format(rotations[i]));
-	//}
-	//
-	//vec4 nVertex[4];
-	//vec4 nNormal[4];
-	//vec4 nVertex_f = vec4(0.0, 0.0, 0.0, 0.0);
-	//vec4 nNormal_f = vec4(0.0, 0.0, 0.0, 0.0);
-	//
-	//for(int i = 0; i < 4; ++i)
-	//{
-	//	int boneChain[MAX_BONES];
-	//	boneChain[0] = index[i];
-	//	int boneCount = 1;
-	//	int pIndex = parentIndices[index[i]];
-	//	
-	//	for (int j = 0; j < MAX_BONES - 1; ++j)
-	//	{
-	//		boneChain[j + 1] = pIndex;
-	//		boneCount += int(step(float(-1), float(pIndex)));
-	//		pIndex = parentIndices[pIndex];
-	//	}
-	//	
-	//	nVertex[i] = vec4(vertex, 0.0);
-	//	nNormal[i] = vec4(normal, 0.0);
-	//	
-	//	for (int j = 0; j < MAX_BONES; ++j)
-	//	{
-	//		int cbIndex = MAX_BONES - j - 1; // current bone Index
-	//		vec4 aBonePos = vec4(bonePos[boneChain[cbIndex]] + (offsets[boneChain[cbIndex]] * step(float(boneCount - 1), float(cbIndex))) * step(float(-1), float(boneChain[cbIndex])), 0.0);
-	//		nVertex[i] -= aBonePos;
-	//		nVertex[i] = boneMat[boneChain[cbIndex]] * nVertex[i];
-	//		nNormal[i] = boneMat[boneChain[cbIndex]] * nNormal[i];
-	//		nVertex[i] += aBonePos;
-	//		nVertex[i] += vec4(offsets[boneChain[cbIndex]] * step(float(boneCount - 1), float(cbIndex)), 0.0);
-	//	}
-	//	
-	//	nVertex_f += nVertex[i] * weight[i];
-	//	nNormal_f += nNormal[i] * weight[i];
-	//}
 	
 	gl_Position = (camera * vec4(nVertex_f.xyz + objectPos, 1.0));
 	normal0 = nNormal_f.xyz;
 	texCoord0 = UV;
 	worldPos0 = nVertex_f.xyz;
-	
-	//gl_Position = (camera * vec4(vertex + objectPos, 1.0));
-	//normal0 = normal;
-	//texCoord0 = UV;
-	//worldPos0 = vertex;
 }
